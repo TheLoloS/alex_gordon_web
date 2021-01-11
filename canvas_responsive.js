cacb579d28e67fb907271a9082d87befceb84b13
TheLoloS <57485545+TheLoloS@users.noreply.github.com>
2021-01-11 04:14:28 +0100

    V 1.0.0.5
    Add menu
    add buttons
    add video to background
    add comentary

---
+++ canvas_responsive.js
@@ -0,0 +1,18 @@
+
+var canvas = document.querySelector("#my-app");
+var ctx = canvas.getContext("2d");
+
+// const video = document.querySelector("video");
+// video.addEventListener("play", () => {
+//   function step() {
+//     ctx.drawImage(
+//       video,
+//       0,
+//       0,
+//       document.body.clientWidth,
+//       document.body.clientHeight
+//     );
+//     requestAnimationFrame(step);
+//   }
+//   requestAnimationFrame(step);
+// });
--- script.js
+++ script.js
@@ -3,11 +3,12 @@ var tab_run =  ["images/alex_run/run1.png", "images/alex_run/run2.png", "images/
 "images/alex_run/run7.png", "images/alex_run/run8.png", "images/alex_run/run9.png", 
 "images/alex_run/run10.png", "images/alex_run/run11.png", "images/alex_run/run12.png", 
 "images/alex_run/run13.png", "images/alex_run/run14.png", "images/alex_run/run15.png", "images/alex_run/run16.png", ]
+
 function init()
 {
     canvas = document.getElementById("my-app");
-    canvas.width = document.body.clientWidth; //document.width is obsolete
-    canvas.height = document.body.clientHeight; //document.height is obsolete
+    canvas.width = document.body.clientWidth; 
+    canvas.height = document.body.clientHeight; 
     canvasW = canvas.width;
     canvasH = canvas.height;
 
@@ -17,22 +18,36 @@ window.onload = function() {
     window.addEventListener('resize', init());
     var myAudio = document.createElement("audio");
     myAudio.loop = true;
-    myAudio.src = "./music/music/menu.mp3";
+    myAudio.src = "./music/music/theme5.mp3";
     myAudio.play();
     var runfr = 0
     var canvas = document.querySelector("#my-app");
     var ctx = canvas.getContext("2d");
-        window.setInterval(function frate (){
-            var img = document.getElementById("alex");
-            img.setAttribute("src", tab_run[runfr])
-            if(runfr != 0){
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-            }
-            ctx.drawImage(img, 100, 100);
-            runfr++
-            if(runfr == 16){
-                runfr = 0
-            }
+    
+    var mnl = document.getElementById("mnl");
+    var mnb1 = document.getElementById("mnb1");
+    var mnb2 = document.getElementById("mnb2");
+    var mnb3 = document.getElementById("mnb3");
+    ctx.drawImage(mnl, document.body.clientWidth - mnl.width +200, 0, 500, 500 * mnl.height / mnl.width)
+    ctx.drawImage(mnb1, document.body.clientWidth - mnb1.width +150, mnl.height - 100, 400, 450 * mnb1.height / mnb1.width)
+    ctx.drawImage(mnb2, document.body.clientWidth - mnb2.width +200, mnl.height + 50, 300, 450 * mnb2.height / mnb2.width)
+    ctx.drawImage(mnb3, document.body.clientWidth - mnb3.width +200, mnl.height + mnb1.height + 40, 300, 450 * mnb3.height / mnb3.width)
+
+    //hero frame rate scipt
+    
+    // window.setInterval(function frate (){
+        //     var img = document.getElementById("alex");
+        //     img.setAttribute("src", tab_run[runfr])
+        //     if(runfr != 0){
+        //         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //     }
+        //     ctx.drawImage(img, 100, 100);
+        //     runfr++
+        //     if(runfr == 16){
+        //         runfr = 0
+        //     }
             
-        }, 60);
+        // }, 60);
+        
+
   };
